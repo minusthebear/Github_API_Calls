dc9dd2dd8e023dd98b682abab46eaffbc426bf14
matthewhamann <matt.hamann1982@gmail.com>
2017-04-02 14:20:17 -0700

    Added more old specs

---
+++ javascripts/oldSpecFiles/api.factory.spec.js
@@ -0,0 +1,128 @@
+"use strict";
+
+// Add error handling checks soon
+
+describe("API Factory", function(){
+
+	var APIFactory, $httpBackend, $q, $provide, $rootScope, result;
+
+	const addy = "https://api.github.com/users/minusthebear";
+
+	const RESPONSE_ERROR = {
+		"message": "Not Found",
+		"documentation_url": "https://developer.github.com/v3"
+	};
+
+	beforeEach(angular.mock.module("app"));
+
+	beforeEach(inject(function(_APIFactory_, _$httpBackend_, _$q_, _$rootScope_){
+		APIFactory = _APIFactory_;
+		$httpBackend = _$httpBackend_;
+		$q = _$q_;
+		$rootScope = _$rootScope_;
+		$rootScope.$new();
+	}));
+
+	afterEach(function() {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+	describe("API Factory should be defined", function(){
+
+		it("API Factory should be defined", function(){
+			expect(APIFactory).toBeDefined();
+		});
+
+	});
+
+	describe("API Factory's Methods", function(){
+		var spy, result;
+
+		beforeEach(function(){
+			spyOn(APIFactory, "getAPI");
+			result = {};
+		});
+
+		it("API Factory's methods should be defined", function(){
+			expect(APIFactory.getAPI).toBeDefined();
+			expect(APIFactory.getAPI).not.toHaveBeenCalled();
+		});
+
+		describe("APIFactory.getAPI() is called", function(){
+			it("API Factory should have been called", function(){
+				let API = APIFactory.getAPI();
+
+				expect(APIFactory.getAPI).toHaveBeenCalled();
+				$rootScope.$apply();
+			});
+		});
+	});
+
+
+	describe("APIFactory should return some values", function(){
+		var spy, result;
+
+		beforeEach(function(){
+			spyOn(APIFactory, "getAPI").and.callThrough();
+			result = {};
+		});
+
+		it("should make an API call and return the correct value", inject(function($http){
+			$httpBackend.whenGET(addy).respond(200, {"id": 8847098});
+
+			expect(APIFactory.getAPI).not.toHaveBeenCalled();
+			expect(result).toEqual({});
+
+			APIFactory.getAPI("minusthebear").then(function(res){
+				result = res;
+			});
+
+			$httpBackend.flush();
+
+			expect(APIFactory.getAPI).toHaveBeenCalled();
+			expect(result.data.id).toEqual(8847098);
+		}));
+
+		it("should return Not Found message with invalid user", function(){
+			$httpBackend.whenGET("https://api.github.com/users/ffeqfenqfewqoio")
+				.respond(404, RESPONSE_ERROR);
+
+			expect(APIFactory.getAPI).not.toHaveBeenCalled();
+			expect(result).toEqual({});
+
+			APIFactory.getAPI("ffeqfenqfewqoio").then(function(res){
+				result = res;
+			}).catch(function(res){
+				result = res;
+			});
+
+			$httpBackend.flush();
+
+			expect(result.data).toEqual(RESPONSE_ERROR);
+		});
+
+	});
+
+	const RESPONSE_SUCCESS = {
+		"login": "minusthebear",
+		"id": 8847098,
+		"avatar_url": "https://avatars1.githubusercontent.com/u/8847098?v=3",
+		"html_url": "https://github.com/minusthebear",
+		"followers_url": "https://api.github.com/users/minusthebear/followers",
+		"following_url": "https://api.github.com/users/minusthebear/following{/other_user}",
+		"repos_url": "https://api.github.com/users/minusthebear/repos",
+		"name": null,
+		"company": null,
+		"blog": null,
+		"location": null,
+		"email": null,
+		"bio": null,
+		"public_repos": 14,
+		"followers": 1,
+		"following": 1,
+		"created_at": "2014-09-21T01:35:11Z",
+		"updated_at": "2017-02-03T20:12:43Z"
+	};
+
+});
---
+++ javascripts/oldSpecFiles/user.factory.spec.js
@@ -0,0 +1,65 @@
+"use strict";
+
+describe("User Factory", function(){
+	var UserFactory;
+
+	beforeEach(angular.mock.module("app"));
+
+	beforeEach(inject(function(_UserFactory_){
+		UserFactory = _UserFactory_;
+	}));
+
+	it("UserFactory and its methods should be defined", function(){
+		expect(UserFactory).toBeDefined();
+		expect(UserFactory.User).not.toBeDefined();
+		expect(UserFactory.setUser).toBeDefined();
+		expect(UserFactory.getUser).toBeDefined();
+	});
+
+	it("should set User and return User", function(){
+		expect(UserFactory.User).not.toEqual(RESPONSE_SUCCESS.data);
+		
+		UserFactory.setUser(RESPONSE_SUCCESS.data);
+		
+		expect(UserFactory.User.id).toEqual(RESPONSE_SUCCESS.data.id);
+		expect(UserFactory.User.links.followers_url).toEqual(UserFactory.User.links.followers_url);
+	});
+
+	it("should take data and return the correct value", function(){
+		UserFactory.setUser(RESPONSE_SUCCESS.data);
+		let x = UserFactory.getUser();
+		expect(x.id).toEqual(8847098);
+		expect(x.links.followers_url).toEqual("https://api.github.com/users/minusthebear/followers");
+	});
+
+	it("should give invalid value undefined", function(){
+		delete RESPONSE_SUCCESS.data.login;
+		UserFactory.setUser(RESPONSE_SUCCESS.data);
+		let x = UserFactory.getUser(RESPONSE_SUCCESS.data);
+		expect(x.login).toEqual(undefined);
+	});
+
+	const RESPONSE_SUCCESS = {
+		status: 200,
+		data: {
+			"login": "minusthebear",
+			"id": 8847098,
+			"avatar_url": "https://avatars1.githubusercontent.com/u/8847098?v=3",
+			"html_url": "https://github.com/minusthebear",
+			"followers_url": "https://api.github.com/users/minusthebear/followers",
+			"following_url": "https://api.github.com/users/minusthebear/following{/other_user}"
+			"name": null,
+			"company": null,
+			"blog": null,
+			"location": null,
+			"email": null,
+			"bio": null,
+			"public_repos": 14,
+			"followers": 1,
+			"following": 1,
+			"created_at": "2014-09-21T01:35:11Z",
+			"updated_at": "2017-02-03T20:12:43Z"		
+		}
+	};
+
+});
